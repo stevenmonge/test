752488d9222ba43f45139ee30754472359873666
stevenmonge <steven94monge@gmail.com>
2019-11-20 17:32:17 -0600

    ayuda

--- fsdfdf.py
+++ fsdfdf.py
@@ -1,3 +1,6 @@
 '''
-hola a toodos
+Entradas: una matriz cuadrada
+Salidasdebo crear una funcion que me intercambie los elementos de las diagonales
+se intercambian los elementos de las diagonales
+Restricciones: (se omite la validacion de restricciones
 '''
