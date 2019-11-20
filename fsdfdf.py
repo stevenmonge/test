75755bd0d8ef0c9d3d7f0f19fd4e50a676e6ce36
stevenmonge <steven94monge@gmail.com>
2019-11-20 17:40:35 -0600

    sfd

--- fsdfdf.py
+++ fsdfdf.py
@@ -4,3 +4,4 @@ Salidasdebo crear una funcion que me intercambie los elementos de las diagonales
 se intercambian los elementos de las diagonales
 Restricciones: (se omite la validacion de restricciones
 '''
+# escribir bien el enunciado tal como aparece q no se entiende mucho
